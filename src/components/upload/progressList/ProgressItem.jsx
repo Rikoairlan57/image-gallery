a86e154eb65e89132e1c4f786432a29ec36f2961
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-04 04:53:34 +0700

    fix

--- src/components/upload/progressList/ProgressItem.jsx
+++ src/components/upload/progressList/ProgressItem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { CheckCircleOutline } from "@mui/icons-material";
 import { Box, ImageListItem } from "@mui/material";
 import React, { useEffect, useState } from "react";
