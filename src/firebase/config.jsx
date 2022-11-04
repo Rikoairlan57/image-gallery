b7e48f57d9fbfe5ed99eb5a0a279c0a8151e1e81
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-04 09:32:34 +0700

    fix

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -21,10 +21,3 @@ export const app = initializeApp(firebaseConfig);
 export const storage = getStorage();
 export const db = getFirestore();
 export const auth = getAuth();
-
-// apiKey: "AIzaSyDtPL7slscQ-A5HIbsW9FifuyqIewxf-2M",
-// authDomain: "website-gallery-48a33.firebaseapp.com",
-// projectId: "website-gallery-48a33",
-// storageBucket: "website-gallery-48a33.appspot.com",
-// messagingSenderId: "451067698304",
-// appId: "1:451067698304:web:372fdcf048c5b44db01a65",
