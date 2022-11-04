5a36dfc72e2831739e5538fba947876015c7d64b
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-04 09:25:05 +0700

    fix protected api

--- src/components/imagesList/Options.jsx
+++ src/components/imagesList/Options.jsx
@@ -1,18 +1,19 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Tooltip from '@mui/material/Tooltip';
-import { IconButton } from '@mui/material';
-import { Delete, MoreVert } from '@mui/icons-material';
-import deleteDocument from '../../firebase/deleteDocument';
-import deleteFile from '../../firebase/deleteFile';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Tooltip from "@mui/material/Tooltip";
+import { IconButton } from "@mui/material";
+import { Delete, Download, MoreVert } from "@mui/icons-material";
+import deleteDocument from "../../firebase/deleteDocument";
+import deleteFile from "../../firebase/deleteFile";
+import { useAuth } from "../../context/AuthContext";
 
-export default function Options({ imageId }) {
+export default function Options({ imageId, uid, imageURL }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
-  const currentUser = { uid: 'userId' };
+  const { currentUser, setAlert } = useAuth();
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -21,25 +22,54 @@ export default function Options({ imageId }) {
   };
   const handleDelete = async () => {
     try {
-      await deleteDocument('gallery', imageId);
+      await deleteDocument("gallery", imageId);
       await deleteFile(`gallery/${currentUser.uid}/${imageId}`);
     } catch (error) {
-      alert(error.message);
+      setAlert({
+        isAlert: true,
+        severity: "error",
+        message: error.message,
+        timeout: 8000,
+        location: "main",
+      });
+      console.log(error);
+    }
+  };
+
+  const handleDownload = async () => {
+    try {
+      const response = await fetch(imageURL);
+      const data = await response.blob();
+      const blob = URL.createObjectURL(data);
+      const link = document.createElement("a");
+      link.href = blob;
+      link.download = imageId;
+      link.click();
+      URL.revokeObjectURL(blob);
+      link.remove();
+    } catch (error) {
+      setAlert({
+        isAlert: true,
+        severity: "error",
+        message: error.message,
+        timeout: 8000,
+        location: "main",
+      });
       console.log(error);
     }
   };
   return (
     <React.Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
         <Tooltip title="Options">
           <IconButton
             onClick={handleClick}
             sx={{
-              position: 'absolute',
+              position: "absolute",
               right: 0,
               top: 0,
-              color: 'white',
-              background: 'rgba(0,0,0,.3)',
+              color: "white",
+              background: "rgba(0,0,0,.3)",
             }}
           >
             <MoreVert fontSize="large" />
@@ -54,38 +84,46 @@ export default function Options({ imageId }) {
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            '& .MuiAvatar-root': {
+            "& .MuiAvatar-root": {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
             },
-            '&:before': {
+            "&:before": {
               content: '""',
-              display: 'block',
-              position: 'absolute',
+              display: "block",
+              position: "absolute",
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
               zIndex: 0,
             },
           },
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
-        <MenuItem onClick={handleDelete}>
+        <MenuItem onClick={handleDownload}>
           <ListItemIcon>
-            <Delete />
+            <Download />
           </ListItemIcon>
-          Delete
+          Download
         </MenuItem>
+        {currentUser?.uid === uid && (
+          <MenuItem onClick={handleDelete}>
+            <ListItemIcon>
+              <Delete />
+            </ListItemIcon>
+            Delete
+          </MenuItem>
+        )}
       </Menu>
     </React.Fragment>
   );
--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,18 +1,30 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDtPL7slscQ-A5HIbsW9FifuyqIewxf-2M",
-  authDomain: "website-gallery-48a33.firebaseapp.com",
-  projectId: "website-gallery-48a33",
-  storageBucket: "website-gallery-48a33.appspot.com",
-  messagingSenderId: "451067698304",
-  appId: "1:451067698304:web:372fdcf048c5b44db01a65",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
+export const storage = getStorage();
 export const db = getFirestore();
 export const auth = getAuth();
+
+// apiKey: "AIzaSyDtPL7slscQ-A5HIbsW9FifuyqIewxf-2M",
+// authDomain: "website-gallery-48a33.firebaseapp.com",
+// projectId: "website-gallery-48a33",
+// storageBucket: "website-gallery-48a33.appspot.com",
+// messagingSenderId: "451067698304",
+// appId: "1:451067698304:web:372fdcf048c5b44db01a65",
